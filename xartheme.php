9242266a1b3a1ba787d536bfd9976c8cd4daeeab
paramss <netspan@paramss.com>
2013-07-26 11:45:06 +0530

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arche Theme
+ *
+ * @package themes
+ * @subpackage arche theme
+ * @category Third Party Xaraya Theme
+ * @version 1.0.0
+ * @copyright 2011 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+ 
+    $themeinfo['name'] = 'arche';
+    $themeinfo['id'] = '30080';
+    $themeinfo['directory'] = 'arche';
+    $themeinfo['author'] = 'Marc Lutolf';
+    $themeinfo['homepage'] = 'http://netspan.ch';
+    $themeinfo['email'] = 'mfl@netspan.ch';
+    $themeinfo['description'] = 'Bermuda Default Theme';
+    $themeinfo['publish_date'] = '01/11/2011';
+    $themeinfo['license'] = 'Public Domain';
+    $themeinfo['version'] = '1.0.0';
+    $themeinfo['xar_version'] = '2.0';
+    $themeinfo['bl_version'] = '2.0';
+    $themeinfo['class'] = '2';
+?>
